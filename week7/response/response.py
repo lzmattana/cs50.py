0f8bd18714536dc9328ede3201b00a9e6050e597
Leonardo Mattana <l.z.mattana@gmail.com>
2023-11-23 21:13:08 -0300

    week7

---
+++ week7/response/response.py
@@ -0,0 +1,16 @@
+import validators
+
+def main():
+    print(validate(input("What's your email adress?")))
+
+def validate(s):
+    if validators.email(s) == True:
+        return f"Valid"
+    else:
+        return f"Invalid"
+
+
+if __name__ == "__main__":
+    main()
+
+
